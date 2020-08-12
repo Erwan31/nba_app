670649a0888dcd3af659fe5dc3ff3eb5a05b031f
Erwan31 <spilmonterwan@gmail.com>
2020-08-12 19:21:37 +0100

    Poll + Articles

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import HomeSlider from './slider';
 import Subscriptions from './../utils/subscribe';
 import HomeArticles from './articles';
+import Poll from './../utils/poll';
 
 class Home extends Component {
     state = { 
@@ -12,7 +13,11 @@ class Home extends Component {
             <>
                 <HomeSlider></HomeSlider>
                 <Subscriptions></Subscriptions>
-                <HomeArticles></HomeArticles>
+                <div className="container">
+                    <HomeArticles/>
+                    <Poll/>
+                </div>
+                
             </>
          );
     }
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/home/index';
 import Footer from './components/teams/footer';
 import Header from './components/teams/header';
+import Article from './components/articles/index';
 
 const Routes = () => {
     return ( 
         <BrowserRouter>
         <Header/>
             <Switch>
+                <Route path="/article/:id" component={Article}></Route>
                 <Route path="/" component={Home}></Route>
             </Switch>
         <Footer/>
